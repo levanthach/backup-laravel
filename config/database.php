d985367900227d0dccdb7b258ff8212088357f4f
levanthach <35605044+levanthach@users.noreply.github.com>
2019-09-27 16:12:34 +0700

    update database config

--- config/database.php
+++ config/database.php
@@ -32,8 +32,6 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-    
-    $DATABASE_URL = parse_url(“postgres://rbrwbrytmpaexo:31055483d3eb5c8c97d91cb6387de52448a280cb3a4a6ec598c2e23f2f89e871@ec2-54-243-208-234.compute-1.amazonaws.com:5432/d3lesf6ccojikh”);
 
     'connections' => [
 
@@ -67,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => $DATABASE_URL["url"],
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
